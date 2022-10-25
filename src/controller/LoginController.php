d49caafe7a81264d0f27056059fdff2753fc2b2f
Erik Vidal Jimenez <evidalj168@gmail.com>
2022-10-25 00:17:18 -0500

    session controller

--- src/controller/LoginController.php
+++ src/controller/LoginController.php
@@ -1,14 +1,33 @@
-<?php
-import 
+<?php 
 class LoginController {
-    public __construct() {}
+    function __construct() {
+        session_start();
+    }
 
     public function login($username, $password){
-        $isLogged = false
+        // session_start();
+        $isLogged = false;
         if($username == "evidalj" && $password == "123456"){
+            $_SESSION["user"] = new UserModel("Erik Vidal Jimenez");
             $isLogged = true;
         }
         return $isLogged;
+    }
+
+    public function validateSession() {
+        return isset($_SESSION["user"]);
+    }
+
+    public function logout(){
+        unset($_SESSION["user"]);
+    }
+
+    public function index(){
+        if($this->validateSession()){
+            require('view/HomeView.php');
+        } else {
+            require('view/LoginView.php');
+        }
     }
     
 }
--- src/index.php
+++ src/index.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hola mundo desde docker y php >.<";
+// echo "Hola mundo desde docker y php >.<";
+require("./model/UserModel.php");
+require("./controller/LoginController.php");
+
+$controller = new LoginController;
+// $controller->login("evidalj", "123456");
+$controller->index();
